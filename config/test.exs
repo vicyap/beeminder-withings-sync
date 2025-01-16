030ee27f9d37f596b42698617f4c11da4df213c2
Victor Yap <vicyap24@gmail.com>
2025-01-15 19:17:25 -0600

    Add docker compose

--- config/test.exs
+++ config/test.exs
@@ -9,6 +9,7 @@ config :beeminder_withings_sync, BeeminderWithingsSync.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: 5433,
   database: "beeminder_withings_sync_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
